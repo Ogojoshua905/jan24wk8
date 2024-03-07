256b2a54ea39f358117c7589b0ed9170e9917da5
Ogojoshua905 <joshuaexcellency1@gmail.com>
2024-03-07 19:37:18 +0100

    fix: edited the recommendation para graph

--- bmi/bmi.js
+++ bmi/bmi.js
@@ -3,10 +3,20 @@ const inputs = document.querySelectorAll('input')
 
 function bmiStatus(){
     const weight = inputs[0].value * 1
-    const height = inputs[1].value * 1
+    const height = inputs[1].value /100
 
-    paras[0].innerHTML = '<strong>BMI Result</strong>' + bmi + 'kgm<sup>-2</sup>'
+    const bmi = (weight / height**2).toFixed(2)
 
-    paras[1].innerHTML = bmi > 30 ? '<strong>BMI Status:</strong> Obesity' :
-    bmi > 25 ? '<strong>BMI Status: </strong> Healthy Weight': '<strong>BMI </strong>'
+    paras[0].innerHTML = '<strong>BMI Result: </strong>' + bmi + 'kgm<sup>-2</sup>'
+
+    paras[1].innerHTML = bmi > 30 ? '<strong>BMI Status: </strong> Obesity' :
+    bmi > 25 ? '<strong>BMI Status: </strong> Overweight' :
+    bmi >=18.5? '<strong>BMI Status: </strong> Healthy Weight': '<strong>BMI Status: </strong> Under Weight'
+
+    paras[2].innerHTML = bmi > 30 ? '<strong>BMI Recommendation: </strong> Reducing calories and practicing healthier eating habits' :
+     bmi > 25 ? '<strong>BMI Recommendation: </strong> Choosing healthier foods (whole grains, fruits and vegetables, healthy fats and protein sources)  exercise regularly.' : 
+     bmi >= 18.5 ? '<strong>Keep Up the Good Work and don\'\t desist from your current eating habit</strong>' :
+     '<strong>BMI Recommendation: </strong>  eat a variety of foods that give you the nutrition you need.'
 }
+
+inputs[1].addEventListener('change', bmiStatus )
